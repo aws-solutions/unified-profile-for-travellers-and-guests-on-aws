c0b6663d7444a1d7b08256e2ab2d2ad3c2a4ae89
Geoffroy Rollat <rollatgr@amazon.com>
2023-01-23 09:18:48 -0500

    added cloudfront

--- source/ucp-portal/ucp/deploy.sh
+++ source/ucp-portal/ucp/deploy.sh
@@ -15,16 +15,16 @@ cp src/app/app.constants-env.ts src/app/app.constants.ts
 cat src/app/app.constants.ts
 echo "1-Get infra config for env $env"
 aws s3 cp s3://$bucket/config/ucp-config-$env.json src/app/ucp-config.json
+contentBucket=$(jq -r .websiteBucket src/app/ind-connector-config.json)
+distributionId=$(jq -r .websiteDistributionId src/app/ind-connector-config.json)
 
 echo "2-Build application package for env $env"
 rm -rf dist
 ng build --prod --aot
-echo "3-Get deploy bucket name from infra config (replace this aws cli call by local json parsing)"
-OutS3BucketAdmin=$(aws cloudformation describe-stacks --stack-name CloudrackInfraWebStack$env --query "Stacks[0].Outputs[?OutputKey=='s3AdministrationPortal'].OutputValue" --output text)
-echo "4-load application to bucket $OutS3BucketAdmin"
-aws s3 sync ./dist/cloudrack-fe-admin s3://$OutS3BucketAdmin --delete
-echo "5-Change local constant file back for local build (this is to be removed after cicd ready)"
+echo "3-load application to bucket $contentBucket"
+aws s3 sync ./dist/rep s3://$contentBucket --delete
+echo "4-Change local constant file back for local build (this is to be removed after cicd ready)"
 cp src/app/app.constants-local.ts src/app/app.constants.ts
-distributionId=$(aws cloudformation describe-stacks --stack-name CloudrackInfraWebStack$env --query "Stacks[0].Outputs[?OutputKey=='cloudrackAdminDistributionId'].OutputValue" --output text)
+echo "5-Invalidate clouudfront distribution $distributionId"
 aws cloudfront create-invalidation --distribution-id $distributionId --path "/*"
 fi
