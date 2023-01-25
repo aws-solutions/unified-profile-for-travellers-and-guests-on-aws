66b3c3ce95bb455cc11b4feef0d2d259372434da
Geoffroy Rollat <rollatgr@amazon.com>
2023-01-25 10:51:02 -0500

    changes for feature pipeline

--- deployment/ucp-code-pipelines/lib/ucp-code-pipelines-stack.ts
+++ deployment/ucp-code-pipelines/lib/ucp-code-pipelines-stack.ts
@@ -244,6 +244,7 @@ export class UCPCodePipelinesStack extends Stack {
               'npm install --legacy-peer-deps',
               'echo "2-Install headless Chrome"',
               'curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -',
+              "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -",
               "echo \"deb http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google-chrome.list",
               'apt-get -y update',
               'apt-get -y install google-chrome-stable',
