6365a34c3410b0e799cce7f627a3e1064c5f03bd
Owen Brady <64945347+owen-brady@users.noreply.github.com>
2023-03-08 16:18:14 -0500

    Move mappings to separate folder, update hotel_booking (#56)

---
+++ source/ucp-backend/src/business-logic/model/accp-mappings/loyalty.go
@@ -0,0 +1,126 @@
+package accpmappings
+
+import customerprofiles "tah/core/customerprofiles"
+
+func BuildLoyaltyMapping() []customerprofiles.FieldMapping {
+	return []customerprofiles.FieldMapping{
+		{
+			Type:   "STRING",
+			Source: "_source.city",
+			Target: "_profile.Address.City",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.state",
+			Target: "_profile.Address.State",
+		},
+		{
+			Type:        "STRING",
+			Source:      "_source.firstName",
+			Target:      "_profile.FirstName",
+			Searcheable: true,
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.ageGroup",
+			Target: "_profile.Attributes.ageGroup",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.honorific",
+			Target: "_profile.Attributes.honorific",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.parentCompany",
+			Target: "_profile.BusinessName",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.points",
+			Target: "_order.Attributes.points",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.gender",
+			Target: "_profile.Gender",
+		},
+		{
+			Type:        "STRING",
+			Source:      "_source.lastName",
+			Target:      "_profile.LastName",
+			Searcheable: true,
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.program",
+			Target: "_order.Attributes.program",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.jobTitle",
+			Target: "_profile.Attributes.jobTitle",
+		},
+		{
+			Type:        "STRING",
+			Source:      "_source.email",
+			Target:      "_profile.EmailAddress",
+			Searcheable: true,
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.email",
+			Target: "_order.Attributes.EmailAddress",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.postalCode",
+			Target: "_profile.Address.PostalCode",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.addressLine1",
+			Target: "_profile.Address.Address1",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.birthdate",
+			Target: "_profile.BirthDate",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.status",
+			Target: "_order.Attributes.status",
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.id",
+			Target: "_profile.AccountNumber",
+			//TODO: this index should go on a dedicated customer ID field
+			Indexes: []string{"UNIQUE", "PROFILE"},
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.id",
+			Target: "_order.Attributes.loyaltyId",
+			//TODO: this index should go on a dedicated customer ID field
+			Indexes: []string{"ORDER"},
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.joined",
+			Target: "_order.Attributes.joined",
+		},
+		{
+			Type:        "STRING",
+			Source:      "_source.phone",
+			Target:      "_profile.PhoneNumber",
+			Searcheable: true,
+		},
+		{
+			Type:   "STRING",
+			Source: "_source.phone",
+			Target: "_order.Attributes.phone",
+		},
+	}
+}
