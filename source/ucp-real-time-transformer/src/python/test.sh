b990d9bc4e139167050c78cd01e9966fafc865f6
Akash Garg <akasgag@amazon.com>
2023-04-04 19:16:31 -0400

    adding other python

--- source/ucp-real-time-transformer/src/python/test.sh
+++ source/ucp-real-time-transformer/src/python/test.sh
@@ -5,6 +5,10 @@ bucket=$2
 echo "Running unit test for python package"
 
 python3 -m unittest discover
+python3 -m coverage run -m unittest discover
+python3 -m coverage xml
+cp coverage.xml ../../../tests/coverage-reports/realtime-coverage.coverage.xml
+rm .coverage coverage.xml
 if [ $? != 0 ]; then
     echo "Python unit tests failed"
     exit 1
