8ecc452de07e5d3653f6e2589f726f7d3ad3af07
Owen Brady <owenmb@amazon.com>
2023-04-06 12:08:24 -0400

    Switch from order to custom profile object types

--- source/ucp-backend/src/business-logic/model/accp-mappings/guest_profile.go
+++ source/ucp-backend/src/business-logic/model/accp-mappings/guest_profile.go
@@ -4,46 +4,13 @@ import customerprofiles "tah/core/customerprofiles"
 
 func BuildGuestProfileMapping() customerprofiles.FieldMappings {
 	return []customerprofiles.FieldMapping{
-		// Metadata
-		{
-			Type:   "STRING",
-			Source: "_source.model_version",
-			Target: "_order.Attributes.model_version",
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.object_type",
-			Target: "_order.Attributes.object_type",
-		},
-		{
-			Type:    "STRING",
-			Source:  "_source.last_updated",
-			Target:  "_order.Attributes.last_updated",
-			Indexes: []string{"UNIQUE", "ORDER"},
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.last_updated_by",
-			Target: "_order.Attributes.last_updated_by",
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.created_on",
-			Target: "_order.Attributes.created_on",
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.created_by",
-			Target: "_order.Attributes.created_by",
-		},
-
 		// Profile Data
 		{
 			Type:        "STRING",
 			Source:      "_source.traveller_id",
 			Target:      "_profile.Attributes.profile_id",
 			Searcheable: true,
-			Indexes:     []string{"PROFILE"},
+			Indexes:     []string{"PROFILE", "UNIQUE"},
 		},
 		{
 			Type:   "STRING",
--- source/ucp-backend/src/business-logic/model/accp-mappings/phone_history.go
+++ source/ucp-backend/src/business-logic/model/accp-mappings/phone_history.go
@@ -4,26 +4,13 @@ import customerprofiles "tah/core/customerprofiles"
 
 func BuildPhoneHistoryMapping() customerprofiles.FieldMappings {
 	return []customerprofiles.FieldMapping{
-		// Metadata
+		// Profile Object Unique Key
 		{
-			Type:   "STRING",
-			Source: "_source.model_version",
-			Target: "_order.Attributes.model_version",
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.object_type",
-			Target: "_order.Attributes.object_type",
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.last_updated",
-			Target: "_order.Attributes.last_updated",
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.last_updated_by",
-			Target: "_order.Attributes.last_updated_by",
+			Type:    "STRING",
+			Source:  "_source.number",
+			Target:  "number",
+			Indexes: []string{"UNIQUE"},
+			KeyOnly: true,
 		},
 
 		// Profile Data
@@ -33,24 +20,6 @@ func BuildPhoneHistoryMapping() customerprofiles.FieldMappings {
 			Target:      "_profile.Attributes.profile_id",
 			Searcheable: true,
 			Indexes:     []string{"PROFILE"},
-		},
-
-		//Order Data
-		{
-			Type:    "STRING",
-			Source:  "_source.number",
-			Target:  "_order.Attributes.number",
-			Indexes: []string{"UNIQUE", "ORDER"},
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.country_code",
-			Target: "_order.Attributes.country_code",
-		},
-		{
-			Type:   "STRING",
-			Source: "_source.type",
-			Target: "_order.Attributes.type",
 		},
 	}
 }
