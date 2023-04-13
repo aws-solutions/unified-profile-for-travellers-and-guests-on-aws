125973f9bbc5c7d252bb445c3659fc2e03f2356d
Owen Brady <owenmb@amazon.com>
2023-04-13 19:54:53 -0400

    Fix regex pattern

--- deployment/ucp-code-pipelines/lib/ucp-code-pipelines-stack.ts
+++ deployment/ucp-code-pipelines/lib/ucp-code-pipelines-stack.ts
@@ -61,7 +61,7 @@ export class UCPCodePipelinesStack extends Stack {
     const partitionStartDate = new CfnParameter(this, "partitionStartDate", {
       type: "String",
       description: "Date to start ingesting customer records from",
-      allowedPattern: "^\d{4}\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])$",
+      allowedPattern: "^(?!0000)[0-9]{4}\/(0?[1-9]|1[0-2])\/(0?[1-9]|[1-2][0-9]|3[0-1])$",
       constraintDescription: "Date with yyyy/mm/dd format",
     });
 
